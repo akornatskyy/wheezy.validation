0d14cc75e57180d403cb266c942ceca4dc1377b4
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-11-10 00:45:49 +0200

    Introduced make target qa; fixed issues reported.

--- src/wheezy/validation/__init__.py
+++ src/wheezy/validation/__init__.py
@@ -2,6 +2,8 @@
 """ ``validation`` package.
 """
 
+# flake8: noqa
+
 from wheezy.validation.mixin import ValidationMixin
 from wheezy.validation.model import try_update_model
 from wheezy.validation.validator import Validator
