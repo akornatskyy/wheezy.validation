aefce46d761a8ca284c4dde384e8ba79c07b3ee5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-03-21 13:35:28 +0200

    Added pargma nocover.

--- src/wheezy/validation/patches.py
+++ src/wheezy/validation/patches.py
@@ -6,13 +6,13 @@
 def patch_strptime_cache_size(max_size=100):
     """ Patch for strptime regex cache max size.
     """
-    try:
+    try:  # pragma: nocover
         import _strptime
         if not hasattr(_strptime, '_CACHE_MAX_SIZE'):
             return False
         if not hasattr(_strptime, '_cache_lock'):
             return False
-    except (ImportError, AttributeError):
+    except (ImportError, AttributeError):  # pragma: nocover
         return False
 
     l = _strptime._cache_lock
