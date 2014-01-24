a8a9c3ec87bb842f2e411221e037665487f47ed5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2014-01-24 22:45:38 +0200

    Added test cases for ErrorMixin.

--- src/wheezy/validation/tests/test_mixin.py
+++ src/wheezy/validation/tests/test_mixin.py
@@ -4,22 +4,29 @@
 
 import unittest
 
+from wheezy.validation.mixin import ErrorsMixin
 from wheezy.validation.mixin import ValidationMixin
 
 
-class MyService(ValidationMixin):
+class MyServiceA(ErrorsMixin):
+
+    def __init__(self):
+        self.errors = {}
+
+
+class MyServiceB(ValidationMixin):
 
     def __init__(self):
         self.errors = {}
         self.translations = {'validation': None}
 
 
-class ValidationMixinTestCase(unittest.TestCase):
+class ErrorMixinTestCase(unittest.TestCase):
 
     def test_errors(self):
         """ Ensure error summary.
         """
-        s = MyService()
+        s = MyServiceA()
         s.error('error-message-1')
         assert {'__ERROR__': ['error-message-1']} == s.errors
 
@@ -27,6 +34,27 @@ class ValidationMixinTestCase(unittest.TestCase):
         assert [('__ERROR__', [
             'error-message-1',
             'error-message-2'])] == sorted(s.errors.items())
+
+        s.error('error-message-3', 'name')
+        assert ['__ERROR__', 'name'] == sorted(s.errors.keys())
+
+
+class ValidationMixinTestCase(unittest.TestCase):
+
+    def test_errors(self):
+        """ Ensure error summary.
+        """
+        s = MyServiceB()
+        s.error('error-message-1')
+        assert {'__ERROR__': ['error-message-1']} == s.errors
+
+        s.error('error-message-2')
+        assert [('__ERROR__', [
+            'error-message-1',
+            'error-message-2'])] == sorted(s.errors.items())
+
+        s.error('error-message-3', 'name')
+        assert ['__ERROR__', 'name'] == sorted(s.errors.keys())
 
     def test_validate(self):
         """ Ensure errors is updated by validator.
@@ -37,7 +65,7 @@ class ValidationMixinTestCase(unittest.TestCase):
             'name': [required]
         })
         user = {'name': 'john'}
-        s = MyService()
+        s = MyServiceB()
         assert s.validate(user, v)
         assert not s.errors
 
